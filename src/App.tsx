78df652452de84f83d2ad7a4d8a0b0fe93239c21
Khant-Nyar-Ko-Ko <kokhantnyar19@gmail.com>
2024-08-17 20:50:17 +0700

    auth layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { useRoutes } from "react-router-dom";
 import PatientRouter from "./router/PatientRouter"
+import AuthRouter from "./router/AuthRouter";
 
 const App = () => {
 
-  const routes = [...PatientRouter];
+  const routes = [...PatientRouter, ...AuthRouter];
   const routing = useRoutes(routes);
 
   return (
