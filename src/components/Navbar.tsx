972ff8975c897cd33d01810b0655dd4bc21847c0
Khant-Nyar-Ko-Ko <kokhantnyar19@gmail.com>
2024-08-17 20:44:36 +0700

    naming

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 const Navbar = () => {
   return (
     <nav className=" flex w-screen justify-between items-center sticky top-0 left-0 px-6 py-4 bg-white shadow-sm select-none">
-      <h2 className="text-primary text-2xl font-semibold">M-Care</h2>
+      <h2 className="text-primary text-2xl font-semibold">Healing Hub</h2>
       <div className=" flex items-center gap-10">
         <button className="text-primary">Home</button>
         <button className="text-primary">Chat</button>
