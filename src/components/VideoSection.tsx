0c2af3a22eba21eead31931114dc8564a771255d
Khant-Nyar-Ko-Ko <kokhantnyar19@gmail.com>
2024-08-18 00:56:27 +0700

    home page

---
+++ src/components/VideoSection.tsx
@@ -0,0 +1,119 @@
+import VideoCard from "./VideoCard";
+import image1 from "../assets/thumbnail/image.png";
+import image2 from "../assets/thumbnail/image2.png";
+import image3 from "../assets/thumbnail/image3.png";
+import image4 from "../assets/thumbnail/image4.png";
+import image5 from "../assets/thumbnail/image5.png";
+import image6 from "../assets/thumbnail/image6.png";
+import creator1 from "../assets/creator/5957bf81479411.5d00ccdc8d19e.jpg";
+import creator2 from "../assets/creator/images (1).jpeg";
+import creator3 from "../assets/creator/images (1).png";
+import creator4 from "../assets/creator/images.jpeg";
+import creator5 from "../assets/creator/images.png";
+
+import preview1 from "../assets/video/health1.mp4";
+import preview2 from "../assets/video/health2.mp4";
+import preview3 from "../assets/video/health3.mp4";
+
+const data = [
+  {
+    id: "1",
+    title: "Understanding Anxiety: Causes and Solutions",
+    channel: {
+      name: "Mind Matters",
+      profileImage: creator1,
+    },
+    thumbnail: image1,
+    duration: "11:45",
+    previewVideo: preview1,
+  },
+  {
+    id: "2",
+    title: "5 Ways to Improve Your Mental Well-being",
+    channel: {
+      name: "Healthy Minds",
+      profileImage: creator2,
+    },
+    thumbnail: image2,
+    duration: "11:32",
+    previewVideo: preview2,
+  },
+  {
+    id: "3",
+    title: "Overcoming Depression: A Personal Journey",
+    channel: {
+      name: "Mental Health Journey",
+      profileImage: creator3,
+    },
+    thumbnail: image3,
+    duration: "32:00",
+    previewVideo: preview3,
+  },
+  {
+    id: "4",
+    title: "Mindfulness Meditation for Stress Relief",
+    channel: {
+      name: "Calm Mind",
+      profileImage: creator4,
+    },
+    thumbnail: image4,
+    duration: "18:23",
+    previewVideo: preview1,
+  },
+  {
+    id: "5",
+    title: "How to Build Resilience and Bounce Back from Setbacks",
+    channel: {
+      name: "Positive Vibes",
+      profileImage: creator5,
+    },
+    thumbnail: image5,
+    duration: "08:44",
+    previewVideo: preview2,
+  },
+  {
+    id: "6",
+    title: "The Science Behind Mental Health: What You Need to Know",
+    channel: {
+      name: "Brain Health",
+      profileImage: creator4,
+    },
+    thumbnail: image6,
+    duration: "12:42",
+    previewVideo: preview3,
+  },
+  {
+    id: "7",
+    title: "Coping with Social Anxiety in a Post-Pandemic World",
+    channel: {
+      name: "Mental Health Awareness",
+      profileImage: creator2,
+    },
+    thumbnail: image1,
+    duration: "05:42",
+    previewVideo: preview2,
+  },
+  {
+    id: "8",
+    title: "The Importance of Sleep for Mental Health",
+    channel: {
+      name: "Sleep Well",
+      profileImage: creator3,
+    },
+    thumbnail: image2,
+    duration: "12:32",
+    previewVideo: preview1,
+  },
+];
+
+const VideoSection = () => {
+  return (
+    <div className="grid items-center justify-center grid-cols-4 gap-10 px-[140px]">
+      {data?.map((item) => (
+        <VideoCard key={item?.id} item={item} />
+      ))}
+    </div>
+  );
+};
+
+export default VideoSection;
--- src/pages/patient/HomePage.tsx
+++ src/pages/patient/HomePage.tsx
@@ -1,7 +1,29 @@
+import Banner from "@/components/Banner";
+import VideoSection from "@/components/VideoSection";
+import { CiSearch } from "react-icons/ci";
+
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <div className="relative ">
+      <Banner />
+      <div className="relative flex justify-center w-screen py-8">
+        <input
+          type="text"
+          placeholder="Search"
+          className=" border w-[600px] px-4 py-2 rounded-full rel"
+        />
+        <CiSearch
+          className=" absolute right-[450px] top-[44px]"
+          color="gray"
+          size="20px"
+        />
+      </div>
+      <section className=" px-[160px] text-lg text-primary">
+        <h2>Recommendation</h2>
+      </section>
+      <VideoSection />
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
