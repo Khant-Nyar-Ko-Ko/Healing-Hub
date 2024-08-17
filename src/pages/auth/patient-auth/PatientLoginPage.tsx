85a91e64e4d492cac4ebdd0fbfbf401075015313
ThetSweLin <6531503189@lamduan.mfu.ac.th>
2024-08-18 02:14:54 +0700

    need to change clearer image

--- src/pages/auth/patient-auth/PatientLoginPage.tsx
+++ src/pages/auth/patient-auth/PatientLoginPage.tsx
@@ -1,7 +1,17 @@
+import cus_login_image from "../../../assets/cus_login.png"
 
 const PatientLoginPage = () => {
   return (
-    <div>PatientLoginPage</div>
+    <div className="relative">
+      <div className="flex-col w-[60%] justify-center">
+        <img src={cus_login_image} className="w-screen object-cover mt-20" alt="Login Image" />
+      </div>
+      <div className="flex w-[35%] justify-center">
+        <button className="px-6 py-2 text-white rounded-lg bg-primary">Login</button>
+        <button className="px-6 py-2 text-white rounded-lg bg-primary">Sign Up</button>
+      </div>   
+
+    </div>
   )
 }
 
